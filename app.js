14483c77f41e5d2f0b55444022d50a47eeda07c9
Juliana <aragaopintojuli@gmail.com>
2021-11-19 23:05:47 -0300

    grab all requisitions

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = Express();
 app.use(Express.json());
 app.use(cors());
 app.use(timeout('300s'));
+
+app.use('/', (req, res, next) => {
+  console.log('did the request');
+  console.log(req.body);
+  console.log('going to next');
+  next();
+});
+
 app.use(routes);
 
 const PORT = process.env.PORT || 3030;
