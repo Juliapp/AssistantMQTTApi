e3e5ec871a3b751d93927c55de8eed4499971f08
Juliana <aragaopintojuli@gmail.com>
2021-11-19 22:58:11 -0300

    add timeout

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import Express from 'express';
 import routes from './src/routes.js';
 import cors from 'cors';
+import timeout from 'connect-timeout';
 
 const app = Express();
 app.use(Express.json());
 app.use(cors());
+app.use(timeout('300s'));
 app.use(routes);
 
 const PORT = process.env.PORT || 3030;
